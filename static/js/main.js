d8c378bc29235e2d3f7c6b69d5d93ad6228a4c6d
wchan2 <willchan2@gmail.com>
2016-07-16 01:07:47 -0400

    change the name of the project

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,15 @@
 ;(function($) {
-	if (!!window.EventSource) {
-		var source = new EventSource('//localhost:5000/skips');
-		// source.addEventListener('open', function(e) {
-		// 	console.log('connection was opened', e);
-		// }, false);
-		// source.addEventListener('error', function(e) {
-		// 	console.log('connection received an error', e);
-		// 	if (e.readyState == EventSource.CLOSED) {
-		// 		console.log('connection was closed due to an error');
-		// 	}
-		// }, false);
-			source.addEventListener('message', function(event) {
-				console.log(event.data);
-				$('.skips-value').html(event.data.skips);
-		}, false);
-	} else {
-		console.log('your browser does not support server side events');
-	}
+	var socket = new WebSocket("ws://localhost:5000/skips");
+	socket.onmessage = function(event) {
+		console.log(event.data);
+	};
+	// if (!!window.EventSource) {
+	// 	var skipsSource = new EventSource('//localhost:5000/skips');
+	// 	skipsSource.addEventListener('message', function(event) {
+	// 		var data = JSON.parse(event.data);
+	// 		$('.skips-value').text(data.skips);
+	// 	}, false);
+	// } else {
+	// 	console.log('your browser does not support server side events');
+	// }
 })(jQuery);
