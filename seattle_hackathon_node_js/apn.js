9f3117b1dad8e2f12ee52a5bdde2a87df723bee6
wchan2 <willchan2@gmail.com>
2016-07-26 16:55:49 -0400

    update device token for demo

--- seattle_hackathon_node_js/apn.js
+++ seattle_hackathon_node_js/apn.js
@@ -2,7 +2,7 @@
 
 var apn = require('apn');
 
-var device = apn.Device("a2777ec7450d7c070da08bd821e39de019a0597995af539bff1ffdd227dc5ccf");
+var device = apn.Device("f3e42b4dc9ce962753db75ea7733be106d856a84c8fdfe62a987d592144ce240");
 var options = { cert: 'pushcert.pem', key: 'pushcert.pem', production: false };
 var apnConnection = new apn.Connection(options);
 
